cbbae72da66e50f792e67f3bce402faf26240d25
Adri <duvalavud@gmail.com>
2025-09-16 22:21:22 +0200

    sql: add v2-dalle-3-base to model constraint

--- update_model_constraint.sql
+++ update_model_constraint.sql
@@ -1,7 +1,7 @@
--- Update the model constraint to allow new model types
+-- Update the model constraint to include new model types
 ALTER TABLE public.dumbgoodies_renders 
 DROP CONSTRAINT dumbgoodies_renders_model_check;
 
 ALTER TABLE public.dumbgoodies_renders 
 ADD CONSTRAINT dumbgoodies_renders_model_check 
-CHECK (model IN ('v1-seedream', 'v1_5-openai', 'v2-dalle-2', 'v2-logo-composite', 'v2-stable-diffusion-xl'));
+CHECK (model IN ('v1-seedream', 'v1_5-openai', 'v2-dalle-2', 'v2-logo-composite', 'v2-dalle-3-base', 'v2-stable-diffusion-xl'));
